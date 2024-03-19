002286e8f2e38f93243bd287082129e7b74074c0
Bruno Garcia <bruno,garcia@vrsoft.com.br>
2024-03-19 08:08:37 -0300

    Commit

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,17 @@
+import  { Client } from 'pg'
+
+async function main() {
+
 const express = require('express')
 const app = express()
 const port = 3000
+
+const client = new Client()
+await client.connect()
+
+const res = await client.query('SELECT $1::text as message', ['Db Connection is ok!!'])
+console.log(res.rows[0].message)
+await client.end()
 
 app.get('/',(req,res) =>{
     res.send('Hello Codelab!!')
@@ -9,3 +20,6 @@ app.get('/',(req,res) =>{
 app.listen(port, ()=> {
     console.log(`Example app listening on port ${port}`)
 })
+
+}
+main();
